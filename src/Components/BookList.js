3493173d44cc0f1c7dc17c42724b842a55134225
Utter <utter@fastmail.com>
2020-05-23 12:26:49 +0100

    fix CORS issue and setstate.books.map is not a function problem

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -2,10 +2,9 @@ import React from 'react'
 import axios from 'axios'
 
 // set up axios config defaults where possible - now wherever I call axios these defaults will be used
-axios.defaults.baseURL = "https://web.consonance.app/api/v4/products.json?"; // test the entire URL with params from Talend
+axios.defaults.baseURL = "https://cors-anywhere.herokuapp.com/https://web.consonance.app/api/v4/products.json?"; // test the entire URL with params from Talend
 axios.defaults.headers.common["Authorization"] =
   "Token token=7362f689ab0b43de99ebb53a19ade8a9"; // send the authorisation token in the header
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'; // is this one needed or even helpful?
 
 export default class BookList extends React.Component {
     state = {
@@ -16,7 +15,7 @@ export default class BookList extends React.Component {
         axios.get()
         .then (res => {
             console.log(res);
-            this.setState({ books: res.data.products[0]});
+            this.setState({ books: res.data.products});
         })
     }
 
@@ -24,7 +23,12 @@ export default class BookList extends React.Component {
 render() {
     return (
         <ul>
-            {this.state.books.map(book => <li key={book.id}>{book.name}</li>)}
+            {this.state.books.map(book => (
+            <li key={book.id}>{book.full_title} {book.isbn}</li>
+            
+            
+            
+            ))};
         </ul>
     )
       
